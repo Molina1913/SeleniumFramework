a5b9d9310590982e3ec8c82b1d46c9d8c2d89884
unknown <julian55558@hotmail.com>
2023-03-06 18:07:48 -0500

    SetUp class for driver initialization

---
+++ src/main/java/org/PageObjects/SetUp.java
@@ -0,0 +1,20 @@
+package org.PageObjects;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SetUp {
+
+    public static WebDriver InitializeChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("incognito");
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
+
+
+}
